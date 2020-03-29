8644c9a17fe6636daea06f3880feba5ccc49e14d
tkytama <itotamanegi@gmail.com>
2020-03-29 16:03:59 +0900

    change :ssh_options

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,8 @@
 
 server '118.27.7.218', user: 'Housee', roles: %w{app db web}, port: 52727
 
-set :ssh_options, keys '~/.ssh/id_rsa_
+  set :ssh_options, {
+    keys: %w(~/.ssh/id_rsa),
+    forward_agent: true,
+    auth_methods: %w(publickey)
+  }
