857997a73060f8cad261535d09d51ec0a521388a
tkytama <itotamanegi@gmail.com>
2020-03-29 16:33:50 +0900

    change :ssh_options

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,4 @@
 
 server '118.27.7.218', user: 'Housee', roles: %w{app db web}, port: 52727
 
-  set :ssh_options, {
-    keys: %w(~/.ssh/id_rsa),
-    forward_agent: true,
-    auth_methods: %w(publickey)
-  }
+set :ssh_options, keys: '~/.ssh/conoha_hoge/id_rsa'
