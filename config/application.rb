5e02ca713a37aa33e010d75b01c9b4ab6811d69a
tkytama <itotamanegi@gmail.com>
2020-03-31 00:51:35 +0900

    repair application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module HouseeVer01
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.generateors do |g|
+    config.generators do |g|
       g.test_framework :rspec,
         fixtures: false,
         view_specs: false,
