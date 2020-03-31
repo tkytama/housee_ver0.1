b0509820eadc705948261153a1c2d8061693fa0f
tkytama <itotamanegi@gmail.com>
2020-04-01 00:16:15 +0900

    [fix]rspecでテストを書いてみた
    refs #3 これまでテストを書いていなかったのでeveryday railsのチュートリアルもとにしてテストを書いた。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it "is valid with a lastname, email, and password" do
+    user = User.new(
+      lastname:  "Sumner",
+      email:      "tester@example.com",
+    )
+    expect(user).to_not be_valid
+  end
+  it "is invalid without a lastname" do
+    user = User.new(lastname:nil)
+    user.valid?
+    expect(user.errors[:lastname]).to_not include("can't be blank")
+  end
 
-it "is valid with a first name, last name, email, and password"
+  it "is invalid without a first name"
 
-it "is invalid without a first name"
+  it "is invalid without a last name"
 
-it "is invalid without a last name"
+  it "is invalid without an email address"
 
-it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
 
-it "is invalid with aduplicate email address"
-
-it "returns a user's full name as a string"
+  it "returns a user's full name as a string"
 end
