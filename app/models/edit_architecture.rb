69239ef001950c863cf97156d912716097cfc879
tkytama <itotamanegi@gmail.com>
2020-04-05 00:55:15 +0900

    [fix]edit_architectureモデルの修正
    バりデーションかけるときにscopeを設定（userが異なればarchitectの値が異なってもOK）。

--- app/models/edit_architecture.rb
+++ app/models/edit_architecture.rb
@@ -1,5 +1,5 @@
 class EditArchitecture < ApplicationRecord
-  validates :architect, presence: true, uniqueness: true
+  validates :architect, presence: true, uniqueness:  { scope: :user_id }
   has_one_attached :image
   has_many :edit_rooms
   belongs_to :user
