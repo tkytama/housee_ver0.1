c8ed72e3fb9019c1fb517534aeccd0966d256122
tkytama <itotamanegi@gmail.com>
2020-04-05 22:54:25 +0900

    [add]rspecでテストを書いてみた_9
    architectがない場合は無効な状態であることのテストを記述した。

--- spec/models/edit_architecture_spec.rb
+++ spec/models/edit_architecture_spec.rb
@@ -1,51 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe EditArchitecture, type: :model do
-  it "does not allow dupline project names per user" do
-    user = User.create(
+
+  before do
+    @user = User.create(
       surname: "Joe",
       lastname: "Tester",
       email: "joetester@example.com",
       password: "aaaaaa",
     )
-
-    user.edit_architectures.create(
+    @edit_architecture = @user.edit_architectures.create(
       architect: "Test Project",
-	    edit_profile_id: 1,
+      edit_profile_id: 1,
     )
-
-    new_project = user.edit_architectures.build(
-      architect: "Test Project",
-	    edit_profile_id: 2,
-    )
-
-    new_project.valid?
-    expect(new_project.errors[:architect]).to include("はすでに存在します")
   end
 
-  it "allows two users to share a edit_architecture name" do
-    user = User.create(
-      surname: "Joe",
-      lastname: "Tester",
-      email: "joetester@example.com",
-      password: "aaaaaa"
-    )
+  describe "when two users register" do
+    it "does not allow dupline project names per user" do
+      new_project = @user.edit_architectures.build(
+        architect: "Test Project",
+	      edit_profile_id: 2,
+      )
 
-    user.edit_architectures.create(
-      architect: "Test Project",
-	    edit_profile_id: 1,
-    )
-    other_user = User.create(
-      surname: "Jane",
-      lastname: "Tester",
-      email: "joetester@example.com",
-      password: "aaaaaa"
-    )
-    other_edit_architecture = other_user.edit_architectures.build(
-      architect: "Test Project",
-	    edit_profile_id: 1,
-    )
+      new_project.valid?
+      expect(new_project.errors[:architect]).to include("はすでに存在します")
+    end
 
-    expect(other_edit_architecture).to be_valid
+    it "allows two users to share a edit_architecture name" do
+      other_user = User.create(
+        surname: "Jane",
+        lastname: "Tester",
+        email: "joetester@example.com",
+        password: "aaaaaa"
+      )
+      other_edit_architecture = other_user.edit_architectures.build(
+        architect: "Test Project",
+	       edit_profile_id: 1,
+       )
+       expect(other_edit_architecture).to be_valid
+     end
+   end
+
+  context "without an variable "
+  it "is invalid without an architect" do
+    edit_architecture = EditArchitecture.new(architect: nil)
+    edit_architecture.valid?
+    expect(edit_architecture.errors[:architect]).to include("を入力してください")
   end
 end
--- spec/models/edit_room_spec.rb
+++ spec/models/edit_room_spec.rb
@@ -2,39 +2,36 @@ require 'rails_helper'
 
 RSpec.describe EditRoom, type: :model do
 # 検索文字列に一致するメモを返すこと
-it "returns edit_rooms that match the search term" do
-  user = User.create(
-        surname: "Joe",
-        lastname: "Tester",
-        email: "joetester@example.com",
-        password: "dottle-nouveau-pavilion-tights-furze",
-      )
+  before do
+    @user = User.create(
+      surname: "Joe",
+      lastname: "Tester",
+      email: "joetester@example.com",
+      password: "dottle-nouveau-pavilion-tights-furze",
+    )
 
-      project = user.edit_architectures.create(
-        architect: "Test Project",
-        edit_profile_id: 1,
-      )
-
-  note1 = project.edit_rooms.create(
-    room_comment: "This is the first note.",
-    user_id: project.user_id
-  )
-  note2 = project.edit_rooms.create(
-    room_comment: "This is the second note.",
-    user_id: project.user_id
-  )
-  note3 = project.edit_rooms.create(
-    room_comment: "First, preheat the oven.",
-    user_id: project.user_id
-  )
-
-  expect(EditRoom.search("first")).to include(note1, note3)
-  expect(EditRoom.search("first")).to_not include(note2)
-  expect(EditRoom.search("room_comment")).to be_empty
+    @project = @user.edit_architectures.create(
+      architect: "Test Project",
+      edit_profile_id: 1,
+    )
   end
+  it "returns edit_rooms that match the search term" do
 
+    note1 = @project.edit_rooms.create(
+      room_comment: "This is the first note.",
+      user_id: @project.user_id
+    )
+    note2 = @project.edit_rooms.create(
+      room_comment: "This is the second note.",
+      user_id: @project.user_id
+    )
+    note3 = @project.edit_rooms.create(
+      room_comment: "First, preheat the oven.",
+      user_id: @project.user_id
+    )
 
-
-
-
+    expect(EditRoom.search("first")).to include(note1, note3)
+    expect(EditRoom.search("first")).to_not include(note2)
+    expect(EditRoom.search("room_comment")).to be_empty
+  end
 end
