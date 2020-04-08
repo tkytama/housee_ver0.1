ff64897100527924379054b918476dbebcace951
tkytama <itotamanegi@gmail.com>
2020-04-09 00:34:09 +0900

    [add]rspecでテストを書いてみた_12
    edit_profileのsurname, addressの値が入っていないとエラーが出るテストを書いた。

--- spec/models/edit_profile_spec.rb
+++ spec/models/edit_profile_spec.rb
@@ -7,5 +7,17 @@ RSpec.describe EditProfile, type: :model do
       edit_profile.valid?
       expect(edit_profile.errors[:office_name]).to include("を入力してください")
     end
+
+    it "is without a surname" do
+      edit_profile = EditProfile.new(surname: nil)
+      edit_profile.valid?
+      expect(edit_profile.errors[:surname]).to include("を入力してください")
+    end
+
+    it "is without an address" do
+      edit_profile = EditProfile.new(address: nil)
+      edit_profile.valid?
+      expect(edit_profile.errors[:address]).to include("を入力してください")
+    end
   end
 end
