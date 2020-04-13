edfc5ecfecb3b630985dd10c2f1f8f8209a9f9f9
tkytama <itotamanegi@gmail.com>
2020-04-13 23:49:35 +0900

    [fix] FactoryBotのuser.rbファイルの修正
    ゴミを入力していたため削除

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module HouseeVer01
 
     config.generators do |g|
       g.test_framework :rspec,
+        fixtures: false,
         view_specs: false,
         helper_specs: false,
         routing_specs: false
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,6 @@
 FactoryBot.define do
   factory :user do
-    it "has a valid factory" do
-      expect(FactoryBot.build(:user)).to be_valid
-    end
+
 
   end
 end
