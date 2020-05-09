70202a2494ba4a294f51a0c5f9025ed02669a061
tkytama <itotamanegi@gmail.com>
2020-05-09 20:00:34 +0900

    [fix]デプロイの設定
    deploy/production.rbの編集

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,4 @@
 
 server '118.27.35.32', user: 'Housee', roles: %w{app db web}, port: 52727
 
-set :ssh_options, keys: '~/.ssh/conoha_Housee/id_rsa'
+set :ssh_options, keys: '~/.ssh/conoha_Housee/id_rsa', forward_agent: true # この行を追加しました。
